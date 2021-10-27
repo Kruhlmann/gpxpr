9fe84f98ed7a632bdafaf4d2850c05814d803fd5
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-10-27 15:05:29 +0200

    Rename file handle for clarity

--- src/gpx_parser/io/file/file.py
+++ src/gpx_parser/io/file/file.py
@@ -8,14 +8,14 @@ class File(Device):  # noqa: WPS110
     def __init__(self, path: str, *permissions: int):
         self._path: str = path
         self._permissions: Iterable[int] = permissions
-        self._file_handle: Optional[FileHandle] = None
+        self._handle_pointer: Optional[FileHandle] = None
 
     def __enter__(self) -> FileHandle:  # noqa: D105
-        self._file_handle = FileHandle(self._path, *self._permissions)
-        return self._file_handle
+        self._handle_pointer = FileHandle(self._path, *self._permissions)
+        return self._handle_pointer
 
     def __exit__(self, *_) -> None:  # type: ignore # noqa: D105
-        if self._file_handle is None:
+        if self._handle_pointer is None:
             return
-        self._file_handle.close()
-        self._file_handle = None
+        self._handle_pointer.close()
+        self._handle_pointer = None
