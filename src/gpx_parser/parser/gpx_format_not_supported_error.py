b51df0de3957718a1b383967c32a243acc6a1c5b
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-10-27 14:44:59 +0200

    Make gpxformaterror inherit from parseerror

--- src/gpx_parser/parser/gpx_format_not_supported_error.py
+++ src/gpx_parser/parser/gpx_format_not_supported_error.py
@@ -1,2 +1,5 @@
-class GPXFormatNotSupportedError(ValueError):
-    pass
+from gpx_parser.parser.gpx_parse_error import GPXParseError
+
+
+class GPXFormatNotSupportedError(GPXParseError):
+    pass  # noqa: WPS420,WPS604
