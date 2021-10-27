d04d69419287a9e5ef1a0cedad85b2afdf10d37b
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-10-27 14:37:18 +0200

    Refactor valid parser test

--- tests/gpx_parser/test_parser.py
+++ tests/gpx_parser/test_parser.py
@@ -1,7 +1,12 @@
 from gpx_parser.parser.gpx_file_parser import GPXFileParser
 
+ACCEPTABLE_ERROR_THRESHOLD = 0.1
+
 
 def test_valid_file():
     intervals = list(GPXFileParser("tests/data/valid.gpx").parse())
     assert len(intervals) == 2
-    assert intervals[0].speed_ms * intervals[0].duration_s == intervals[0].distance_m
+    for interval in intervals:
+        expected_distance_m = interval.speed_ms * interval.duration_s
+        distance_delta = abs(expected_distance_m - interval.distance_m)
+        assert distance_delta < ACCEPTABLE_ERROR_THRESHOLD
