d83196854323ecdbb44a0dbe906439e120307777
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-10-28 13:56:11 +0200

    Added colors to stdout renderer

--- src/gpx_renderer/renderers/stdout.py
+++ src/gpx_renderer/renderers/stdout.py
@@ -3,12 +3,18 @@ from typing import Iterator
 
 from gpx_parser.parser.gpx_track_interval import GPXInterval
 from gpx_renderer.renderer import Renderer
+from gpx_renderer.terminal_colors import TerminalColors
 
 
 class STDOUTRenderer(Renderer):
+    def _color_from_pace(self, pace: float) -> TerminalColors:
+        if pace > self._running:
+            return TerminalColors.FAIL
+        if pace > self._walking:
+            return TerminalColors.WARNING
+        return TerminalColors.OKCYAN
+
     def render(self, intervals: Iterator[GPXInterval]) -> None:
         for interval in intervals:
-            if interval.speed_kmtime < self._walking:
-                sys.stdout.write("█")
-            else:
-                sys.stdout.write("x")
+            color = self._color_from_pace(interval.speed_kmtime)
+            sys.stdout.write(f"{color}█{TerminalColors.ENDC}")
